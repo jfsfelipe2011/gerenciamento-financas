ded820951353c98af3a60007b35c6e5a0b104f61
José Felipe da Silva <jfsfelipe2011@gmail.com>
2018-09-03 23:57:45 -0300

    Criando classe que controlara a aplicação

---
+++ src/Application.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace JFin;
+
+class Application
+{
+	/**
+	 * [$serviceContainer Container de serviços]
+	 * @var ServiceContainerInterface
+	 */
+	private $serviceContainer;
+
+	/**
+	 * [Injeção de dependencia de uma interface de Container de serviço]
+	 * 
+	 * @param ServiceContainerInterface $serviceContainer [Interface para container de serviço]
+	 */
+	public function __construct(ServiceContainerInterface $serviceContainer)
+	{
+		$this->serviceContainer = $serviceContainer;
+	}
+
+	/**
+	 * [Busca um serviço no container]
+	 * 
+	 * @param  string $name [nome de um serviço]
+	 * @return mixed        [serviço]
+	 */
+	public function service($name)
+	{
+		return $this->serviceContainer->get($name);
+	}
+
+	/**
+	 * [Adiciona um novo serviço ao container]
+	 * 
+	 * @param string $name    [nome do serviço]
+	 * @param mixed  $service [serviço a ser adicionado]
+	 */
+	public function addService(string $name, $service)
+	{
+		if(is_callable($service)) {
+			$this->serviceContainer->addLazy($name, $service);
+		} else {
+			$this->serviceContainer->add($name, $service);
+		}
+	}
+}
