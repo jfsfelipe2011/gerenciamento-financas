1e521df4f2894755e13e07ce31cad51e429d0d51
José Felipe da Silva <jfsfelipe2011@gmail.com>
2018-09-05 23:24:10 -0300

    Edição de deleção de categoria

--- public/index.php
+++ public/index.php
@@ -36,7 +36,48 @@ $app
 		\JFin\Models\CategoryCost::create($data);
 
 		return $app->route('category-costs.list');
-	}, 'category-costs.store');
+	}, 'category-costs.store')
+	->get('/category-costs/{id}/edit', function (ServerRequestInterface $request) use ($app) {
+		$view = $app->service('view.renderer');
+
+		$id = $request->getAttribute('id');
+		$category = \JFin\Models\CategoryCost::findOrFail($id);
+
+		return $view->render('category-costs/edit.html.twig', [
+			'category' => $category
+		]);
+	}, 'category-costs.edit')
+	->post('/category-costs/{id}/update', function (ServerRequestInterface $request) use ($app) {
+		$view = $app->service('view.renderer');
+
+		$id = $request->getAttribute('id');
+		$category = \JFin\Models\CategoryCost::findOrFail($id);
+
+		$data = $request->getParsedBody();
+		$category->fill($data);
+		$category->save();
+
+		return $app->route('category-costs.list');
+	}, 'category-costs.update')
+	->get('/category-costs/{id}/show', function (ServerRequestInterface $request) use ($app) {
+		$view = $app->service('view.renderer');
+
+		$id = $request->getAttribute('id');
+		$category = \JFin\Models\CategoryCost::findOrFail($id);
+
+		return $view->render('category-costs/show.html.twig', [
+			'category' => $category
+		]);
+	}, 'category-costs.show')
+	->get('/category-costs/{id}/delete', function (ServerRequestInterface $request) use ($app) {
+		$view = $app->service('view.renderer');
+
+		$id = $request->getAttribute('id');
+		$category = \JFin\Models\CategoryCost::findOrFail($id);
+		$category->delete();
+
+		return $app->route('category-costs.list');
+	}, 'category-costs.delete');
 
 
 $app->start();
